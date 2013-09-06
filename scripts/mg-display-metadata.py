3a3a27e5a882678fd6ebf63e2130bad0e2ef66a2
Travis Harrison <guumby@gmail.com>
2013-09-06 12:53:50 -0500

    new script

---
+++ scripts/mg-display-metadata.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import sys
+import urllib
+from operator import itemgetter
+from optparse import OptionParser
+from communities import *
+
+prehelp = """
+NAME
+    mg-display-metadata
+
+VERSION
+    %s
+
+SYNOPSIS
+    mg-display-metadata [ --help, --user <user>, --pass <password>, --token <oAuth token>, --id <metagenome id>, --verbosity <cv: 'mixs', 'full'> ]
+
+DESCRIPTION
+    Retrieve metadata for a metagenome from the communities API.
+"""
+
+posthelp = """
+Output
+    -
+
+EXAMPLES
+    mg-display-metadata --id "kb|mg.287" --verbosity full
+
+SEE ALSO
+    -
+
+AUTHORS
+    %s
+"""
+
+def main(args):
+    OptionParser.format_description = lambda self, formatter: self.description
+    OptionParser.format_epilog = lambda self, formatter: self.epilog
+    parser = OptionParser(usage='', description=prehelp%VERSION, epilog=posthelp%AUTH_LIST)
+    parser.add_option("", "--id", dest="id", default=None, help="KBase Metagenome ID")
+    parser.add_option("", "--url", dest="url", default=API_URL, help="communities API url")
+    parser.add_option("", "--user", dest="user", default=None, help="OAuth username")
+    parser.add_option("", "--passwd", dest="passwd", default=None, help="OAuth password")
+    parser.add_option("", "--token", dest="token", default=None, help="OAuth token")
+    parser.add_option("", "--verbosity", dest="verbosity", default='mixs', help="amount of metadata to display. use keyword 'mixs' for GSC MIxS metadata, use keyword 'full' for all GSC metadata")
+    
+    # get inputs
+    (opts, args) = parser.parse_args()
+    if not opts.id:
+        sys.stderr.write("ERROR: id required\n")
+        return 1
+    
+    # get auth
+    token = get_auth_token(opts)
+    
+    # build call url
+    if opts.id.startswith('kb|'):
+        opts.id = kbid_to_mgid(opts.id)
+    url = opts.url+'/metagenome/'+opts.id+'?verbosity=metadata'
+
+    # retrieve / output data
+    result = obj_from_url(url, auth=token)
+    if opts.verbosity == 'mixs':
+        for m in sorted(result['mixs'].iterkeys()):
+            sys.stdout.write("%s\t%s\n" %(m, result['mixs'][m]))
+    elif opts.verbosity == 'full':
+        md = result['metadata']
+        sys.stdout.write("category\tlabel\tvalue\n")
+        if ('project' in md) and md['project']['data']:
+            for p in sorted(md['project']['data'].iterkeys()):
+                sys.stdout.write("project\t%s\t%s\n" %(p, md['project']['data'][p]))
+        if ('sample' in md) and md['sample']['data']:
+            for s in sorted(md['sample']['data'].iterkeys()):
+                sys.stdout.write("sample\t%s\t%s\n" %(s, md['sample']['data'][s]))
+        if ('library' in md) and ('type' in md['library']) and md['library']['data']:
+            for l in sorted(md['library']['data'].iterkeys()):
+                sys.stdout.write("library: %s\t%s\t%s\n" %(md['library']['type'], l, md['library']['data'][l]))
+        if ('env_package' in md) and ('type' in md['env_package']) and md['env_package']['data']:
+            for e in sorted(md['env_package']['data'].iterkeys()):
+                sys.stdout.write("env package: %s\t%s\t%s\n" %(md['env_package']['type'], e, md['env_package']['data'][e]))
+    else:
+        sys.stderr.write("ERROR: invalid verbosity type\n")
+        return 1
+    
+    return 0
+    
+
+if __name__ == "__main__":
+    sys.exit( main(sys.argv) )
