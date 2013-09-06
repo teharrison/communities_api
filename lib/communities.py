bdcf184000d0d2c93fe847cdc3155b48762d203b
Travis Harrison <guumby@gmail.com>
2013-09-06 11:34:54 -0500

    updated id lookup, fixed some formatting

--- lib/communities.py
+++ lib/communities.py
@@ -9,6 +9,7 @@ ID_URL  = 'http://www.kbase.us/services/idserver'
 OAUTH_URL = 'https://nexus.api.globusonline.org/goauth/token?grant_type=client_credentials'
 AUTH_LIST = "Jared Bischof, Travis Harrison, Folker Meyer, Tobias Paczian, Andreas Wilke"
 
+# return python struct from JSON output of MG-RAST API
 def obj_from_url(url, auth=None, debug=False):
     header = {'Accept': 'application/json'}
     if auth:
@@ -36,7 +37,8 @@ def obj_from_url(url, auth=None, debug=False):
         sys.stderr.write("ERROR (%s): %s\n" %(url, obj['ERROR']))
         sys.exit(1)
     return obj
-    
+
+# print to stdout results of MG-RAST API
 def stout_from_url(url, auth=None, debug=False):
     header = {'Accept': 'text/plain'}
     if auth:
@@ -59,6 +61,7 @@ def stout_from_url(url, auth=None, debug=False):
             break
         sys.stderr.write(chunk)
 
+# transform sparse matrix to dense matrix (2D array)
 def sparse_to_dense(sMatrix, rmax, cmax):
     dMatrix = [[0 for i in range(cmax)] for j in range(rmax)]
     for sd in sMatrix:
@@ -66,12 +69,14 @@ def sparse_to_dense(sMatrix, rmax, cmax):
         dMatrix[r][c] = v
     return dMatrix
 
+# transform BIIOM format to tabbed table
 def biom_to_tab(biom, hdl):
     matrix = sparse_to_dense(biom['data'], biom['shape'][0], biom['shape'][1])
     hdl.write( "\t%s\n" %"\t".join([c['id'] for c in biom['columns']]) )
     for i, row in enumerate(matrix):
-        hdl.write( "%s\t%s\n" %(biom['rows'][i], "\t".join([str(r) for r in row])) )
+        hdl.write( "%s\t%s\n" %(biom['rows'][i]['id'], "\t".join([str(r) for r in row])) )
 
+# return MG-RAST id for given KBase id
 def kbid_to_mgid(kbid):
     id_map = kbid_lookup([kbid])
     if kbid not in id_map:
@@ -79,6 +84,19 @@ def kbid_to_mgid(kbid):
         sys.exit(1)
     return id_map[kbid]
 
+# return list of MG-RAST ids for given list of KBase ids
+#  handels mixed ids / all mgrast ids
+def kbids_to_mgids(kbids):
+    id_map = kbid_lookup(kbids)
+    mgids = []
+    for i in kbids:
+        if i in id_map:
+            mgids.append(id_map[i])
+        else:
+            mgids.append(i)
+    return mgids
+
+# return map (KBase id -> MG-RAST id) for given list of KBase ids
 def kbid_lookup(kbids):
     params = {'method': 'IDServerAPI.kbase_ids_to_external_ids', 'params': [kbids], 'version': '1.1'}
     data = json.dumps(params)
@@ -98,10 +116,10 @@ def kbid_lookup(kbids):
     if 'error' in obj:
         sys.stderr.write("ERROR: %s\n" %obj['error']['message'])
         sys.exit(1)
-    if ('result' not in obj) or (len(obj['result'][0].keys()) == 0):
-        sys.stderr.write("ERROR (%s): no results returned for ids (%s)\n" %(ID_URL, ','.join(kbids)))
-        sys.exit(1)
-    return dict([(k, obj['result'][0][k][1]) for k in obj['result'][0].keys()])
+    if len(obj['result'][0].keys()) == 0:
+        return {}
+    else:
+        return dict([(k, obj['result'][0][k][1]) for k in obj['result'][0].keys()])
 
 def get_auth_token(opts):
     if opts.token:
